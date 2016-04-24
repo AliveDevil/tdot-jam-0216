1eb5a2286372f180c260021cfa54624bc2f1793f
Jöran Malek <Jran Malek>
2016-04-24 12:20:31 +0200

    Added MainMenuPage.

---
+++ Assets/Code/UI/Pages/MainMenuPage.cs
@@ -0,0 +1,25 @@
+﻿using Triconomy.UI;
+using UnityEngine;
+
+public class MainMenuPage : UIView
+{
+	[SerializeField]
+	private UIView gameView;
+	[SerializeField]
+	private UIView highscoreView;
+
+	public void Exit()
+	{
+		Application.Quit();
+	}
+
+	public void OpenHighscore()
+	{
+		UIStack.Push(highscoreView);
+	}
+
+	public void StartGame()
+	{
+		UIStack.Replace(gameView);
+	}
+}
