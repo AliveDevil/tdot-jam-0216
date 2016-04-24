b991dcd2f559c619e8429ceac96f9cfdcc97d398
Daniel Lause <info@daniel-lause.de>
2016-04-24 12:49:00 +0200

    added character Controller and take target position with mouse click

---
+++ Assets/Code/CharacterControl/CharacterController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CharacterController : MonoBehaviour
+{
+    private Vector3 targetPosition;
+
+    public Vector3 TargetPosition
+    {
+        get { return targetPosition; }
+        set { targetPosition = value; }
+    }
+
+    Ray ray;
+    RaycastHit2D hit;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+           hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (hit.transform != null)
+            {
+                targetPosition = hit.point;
+            }
+        }
+    }
+}
