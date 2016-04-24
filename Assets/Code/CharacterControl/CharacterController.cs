569b10df924dabb7c0272d74355eadbe4b1dfcf4
Jöran Malek <Jran Malek>
2016-04-24 17:30:56 +0200

    FUCK.

--- Assets/Code/CharacterControl/CharacterController.cs
+++ Assets/Code/CharacterControl/CharacterController.cs
@@ -1,45 +1,61 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    private Vector3 targetPosition;
+	private PathFindingAgent agent;
+	private RaycastHit2D hit;
+	private Rigidbody2D myRigidBody;
+	private Ray ray;
+	private Vector3 targetPosition;
 
-    public Vector3 TargetPosition
-    {
-        get { return targetPosition; }
-        set { targetPosition = value; }
-    }
+	public Vector3 TargetPosition
+	{
+		get
+		{
+			return targetPosition;
+		}
+		set
+		{
+			targetPosition = value;
+		}
+	}
 
-    private Ray ray;
-    private RaycastHit2D hit;
-    private Rigidbody2D myRigidBody;
+	private void Awake()
+	{
+		agent = GetComponent<PathFindingAgent>();
+		myRigidBody = GetComponent<Rigidbody2D>();
+	}
 
-    private void Awake()
-    {
-        myRigidBody = GetComponent<Rigidbody2D>();
-    }
+	private void FixedUpdate()
+	{
+		moveWithWASD();
+	}
 
-    private void FixedUpdate()
-    {
-        moveWithWASD();
-        moveWithClick();
-    }
+	private void moveWithClick()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+			if (hit.transform != null)
+			{
+				agent.Move(hit.point.ToPoint());
+			}
+		}
+	}
 
-    private void moveWithWASD()
-    {
-        myRigidBody.MovePosition((Vector2)transform.position + new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * Time.deltaTime);
-    }
+	private void moveWithWASD()
+	{
+		var input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		if (input.magnitude > 0)
+		{
+			agent.MovePosition(myRigidBody.position + new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * Time.deltaTime);
+		}
+	}
 
-    private void moveWithClick()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-            if (hit.transform != null)
-            {
-                targetPosition = hit.point;
-                myRigidBody.MovePosition(new Vector2(targetPosition.x, targetPosition.y));
-            }
-        }
-    }
+	private void Update()
+	{
+		moveWithClick();
+	}
 }
