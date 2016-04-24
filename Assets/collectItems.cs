89d7faed49f3b822e987805aed60eab8b76dc0fc
Xellss <Xellsnr1@hotmail.de>
2016-04-24 14:48:44 +0200

    save commit

---
+++ Assets/collectItems.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class collectItems : MonoBehaviour
+{
+
+    [SerializeField]
+    private float time;
+
+    [SerializeField]
+    Text timer;
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "TimeBonus")
+        {
+            time -= 30;
+            if (time < 0)
+            {
+                time = 0;
+            }
+            GameObject.Destroy(other.gameObject);
+        }
+        if (other.gameObject.tag == "TimePenalty")
+        {
+            time += 30;
+            GameObject.Destroy(other.gameObject);
+        }
+    }
+    void Update()
+    {
+        time += 1 * Time.deltaTime;
+        timer.text = time.ToString();
+    }
+}
